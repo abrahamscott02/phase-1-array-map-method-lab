936036d9c384d508c8d6130a107800a16e094480
Abraham Scott <abrahamscott02@gmail.com>
2023-09-26 12:39:01 -0600

    Done

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map((tutorial) => {
+    const words = tutorial.split(" ");
+    const capitalizedWords = words.map((word) => {
+      if (word.length > 0) {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      } else {
+        return word;
+      }
+    });
+    return capitalizedWords.join(" ");
+  });
 }
